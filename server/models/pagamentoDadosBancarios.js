20d3e264f84bf4bcaaf5d60b0c60dbe0ce5d46a5
tawan.gabriel10 <tawan.gabriel10@gmail.com>
2020-07-15 18:42:15 -0300

    Criando Entidade Pagamento Dados bancarios

--- server/models/pagamentoDadosBancarios.js
+++ server/models/pagamentoDadosBancarios.js
@@ -28,38 +28,58 @@ module.exports = (sequelize, DataTypes) => {
     },
     razaoSocial: {
       field: 'razao_social',
-      type: DataTypes.DOUBLE,
+      type: DataTypes.STRING,
       allowNull: true
     },
     cnpj: {
-
+      field: 'cnpj',
+      type: DataTypes.STRING,
+      allowNull: true
     },
     formasDePagamento: {
-
+      field: 'formas_de_pagamento',
+      type: DataTypes.STRING,
+      allowNull: true
     },
     agencia: {
-
+      field: 'agencia',
+      type: DataTypes.STRING,
+      allowNull: true
     },
     contaCorrente: {
-
+      field: 'conta_corrente',
+      type: DataTypes.STRING,
+      allowNull: true
     },
     banco: {
-
+      field: 'banco',
+      type: DataTypes.STRING,
+      allowNull: true
     },
     numeroBanco: {
-
+      field: 'numero_banco',
+      type: DataTypes.INTEGER,
+      allowNull: true
     },
     cidadeAgencia: {
-
+      field: 'cidade_agencia',
+      type: DataTypes.STRING,
+      allowNull: true
     },
     ufConta: {
-
+      field: 'uf_conta',
+      type: DataTypes.STRING,
+      allowNull: true
     },
     modalidade: {
-
+      field: 'modalidade',
+      type: DataTypes.STRING,
+      allowNull: true
     },
     tipoContribuinte: {
-        
+      field: 'tipo_contribuinte',
+      type: DataTypes.STRING,
+      allowNull: true
     }
 }, {
   tableName: 'tb_pagamento_dados_bancarios',

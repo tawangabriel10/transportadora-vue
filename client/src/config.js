9c212637bf029c9a705b0e90ffa436359b5890c8
Tawan Gabriel <tawangabriel@MacBook-Pro-de-Tawan.local>
2020-08-11 01:00:49 -0300

    Apontamento URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 export default {
-  baseUrl: 'http://localhost:5050/i9ty-v1',
-  //baseUrl: 'http://192.241.144.56:5050/i9ty-v1',
+  //baseUrl: 'http://localhost:5050/i9ty-v1',
+  baseUrl: 'http://192.241.144.56:5050/i9ty-v1',
   colors: {
     primary: '#00008B'
   }
